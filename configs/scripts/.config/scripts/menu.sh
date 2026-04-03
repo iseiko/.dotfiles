ce6d679e36ce29b388e83471ca00fda6162e4f90
iseiko <spam.kvncs@gmail.com>
2026-04-03 11:10:57 -0300

    Updating configs

--- configs/scripts/.config/scripts/menu.sh
+++ configs/scripts/.config/scripts/menu.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-chosen=$(printf "Logout\\nShutdown\\nReboot\\nSuspend" | rofi -dmenu -i -p "System" -width 10 -lines 4)
+chosen=$(printf "Logout\\nShutdown\\nReboot\\nSuspend\\nHibernate" | rofi -dmenu -i -p "System" -width 5 -lines 4)
 
 case "$chosen" in
 "Logout") hyprctl dispatch exit ;;
 "Shutdown") systemctl poweroff ;;
 "Reboot") systemctl reboot ;;
 "Suspend") systemctl suspend ;;
+"Hibernate") systemctl hibernate ;;
 esac
